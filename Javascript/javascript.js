11fe2a04b85fd4877175cb0128bfcb5d274e4a6d
Snissysneak <rasmus.dahlberg2000@gmail.com>
2019-05-06 18:58:16 +0200

    hotfix

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,21 +1,33 @@
+var exit = document.getElementById('exit');
+
 var total = 0
+var totalclicks = 0
 var a = 1
 
 var totalwaifuspent = 0
 
+//exit system
+function exit() {
+  exit.addEventListener('click', function() {
+    window.close();
+  });
+}
+
 //money system
 function add() {
   total = total + a
-    document.getElementById("score").value = total + " Waifu's";
-    document.title = total + " Waifu's";
+  totalclicks = totalclicks + 1
+  document.getElementById("score").value = total + " Waifu's";
+  document.getElementById("clicks").value = totalclicks + " Clicks";
+  document.title = total + " Waifu's";
 
-    var waifu = document.getElementById("waifu");
+  var waifu = document.getElementById("waifu");
 
-    waifu.classList.remove('make-bigger');
+  waifu.classList.remove('make-bigger');
 
-    void waifu.offsetWidth;
+  void waifu.offsetWidth;
 
-    waifu.classList.add('make-bigger');
+  waifu.classList.add('make-bigger');
 }
 
 function spent() {
