d90be6561c7940a5659b8347512f83c0b0692863
filipfloden <filip.floden@elev.ga.it-gymnasiet.se>
2019-05-06 11:11:54 +0200

    hotfix

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -3,12 +3,19 @@ var a = 1
 
 var totalwaifuspent = 0
 
-
 //money system
 function add() {
   total = total + a
     document.getElementById("score").value = total + " Waifu's";
     document.title = total + " Waifu's";
+
+    var waifu = document.getElementById("waifu");
+
+    waifu.classList.remove('make-bigger');
+
+    void waifu.offsetWidth;
+
+    waifu.classList.add('make-bigger');
 }
 
 function spent() {
--- main.js
+++ main.js
@@ -8,8 +8,9 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    fullscreen: true,
+    resizable: false,
+    frame: false,
     webPreferences: {
       nodeIntegration: true
     }
